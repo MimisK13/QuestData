1552501be4b1fc13d47820cf3e9f2443ea19fc8c
MimisK <mimisk88@gmail.com>
2020-03-18 20:12:38 +0200

    update cmd

--- htm.cmd
+++ htm.cmd
@@ -1,5 +1,6 @@
-@echo on
+@echo OFF
 	TITLE LineageII - MimisK
-
-	for /r "htm" %%i in (*.htm) do copy "%%~fi" "htm-build\"
+	ECHO "Wait..." 
+	for /r "htm" %%i in (*.htm) do copy "%%~fi" "htm-build\" > nul
+	ECHO "Build has finish!"
 @pause
